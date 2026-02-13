a58d0c0189e4d50eac9bc072c732e18aa333d1e9
NandoGonzales <fg708304@gmail.com>
2026-02-13 18:23:01 +0800

    first

--- dump_settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-// dump_settings.php
-require_once('initialize_coreT2.php');
-$qry = $conn->query("SELECT * FROM system_info");
-while ($row = $qry->fetch_assoc()) {
-    echo $row['meta_field'] . ": " . $row['meta_value'] . "\n";
-}
-?>
--- simple_test.php
+++
@@ -1,4 +0,0 @@
-<?php
-file_put_contents('simple_test.txt', 'PHP is working: ' . date('Y-m-d H:i:s'));
-echo "File written.";
-?>
