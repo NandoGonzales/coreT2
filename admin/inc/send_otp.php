13473ca22c69f1b6a9daec69535fcf5e670018d4
NandoGonzales <fg708304@gmail.com>
2026-02-13 21:03:11 +0800

    first

--- admin/inc/send_otp.php
+++ admin/inc/send_otp.php
@@ -72,13 +72,13 @@ function sendOTPEmail($recipientEmail, $recipientName, $otp) {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'microfinancecore@gmail.com';
-        $mail->Password   = 'xmtjeqdoesrujaom';
+        $mail->Username   = 'fg708304@gmail.com';
+        $mail->Password   = 'morznqwbbcrnrhmc';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
 
         // Recipients
-        $mail->setFrom('microfinancecore@gmail.com', 'CORET2 System');
+        $mail->setFrom('fg708304@gmail.com', 'CORET2 System');
         $mail->addAddress($recipientEmail, $recipientName);
 
         // Email content
