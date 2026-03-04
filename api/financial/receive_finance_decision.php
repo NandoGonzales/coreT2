8ff259aa713c5a255bcf0377025595ff129db171
NandoGonzales <fg708304@gmail.com>
2026-03-04 20:26:18 +0800

    first

--- api/financial/receive_finance_decision.php
+++ api/financial/receive_finance_decision.php
@@ -11,7 +11,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 0);
 date_default_timezone_set('Asia/Manila');
 
-require_once(__DIR__ . '/../initialize_coreT2.php');
+require_once(__DIR__ . '/../../initialize_coreT2.php');
 
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: https://finance.microfinancial-1.com');
